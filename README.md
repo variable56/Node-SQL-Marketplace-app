61ae9d0112073a2969b806ef05134f46aff27de1
variable56 <variable56@gmail.com>
2019-02-04 22:04:59 -0700

    final version

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # Node-SQL-Marketplace-app
+
+##Overview
+
+This is a command line interface program that simulates a retail inventory management software. It takes user inputs on what they want to buy as well as the quantity they want to purchase.  If there is enough stock on hand within the SQL database that the javascript file is connecting to, it will allow the purchase to go through and display a message and total.
+
+##Installation
+
+In order to get the program working on a local computer you will need to create a sql table called "bamazon" that has a "products" table within it.  Below is a list of column names and datatypes for each of the columns:
+
+item_id INT AUTO_INCREMENT PRIMARY KEY
+product_name VARCHAR
+department_name VARCHAR
+price DECIMAL (2 Places)
+stock_quantity INT
+
+##Dependencies
+
+The following npm packages will be installed using the terminal:
+
+-mysql
+-inquirer
+
+you will also need to add a .env file and store your password within the file.  Your .env file should have this as the first line:
+
+password=YOUR_PASSWORD_HERE
+
+##WORKING VERSION
+
+The following is a link of the working version:
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var ctable = require("console.table");
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -21,11 +20,8 @@ loadTable();
 function loadTable() {
     connection.query("SELECT * FROM products", function (error, results) {
         if (error) throw error;
-        // console.log(results);
-        // for (i = 0; i < results.length; i++) {
-        //     var table = ctable.getTable(results);
             console.table(results);
-        // }
+    
         //begins the user prompts using the inquirer package (Object of questions)
         inquirer.prompt([{
             type: "input",
@@ -47,14 +43,14 @@ function loadTable() {
 
             //stores the new value that will update the SQL table with a SET 
             var reduction = (results[idx].stock_quantity) - quantity;
+            var price = results[idx].price;
 
             //Tests the database to see if there is enough of the item to purchase from the database
             if (results[idx].stock_quantity >= quantity) {
                 connection.query("UPDATE products SET stock_quantity = ? WHERE item_id = ?;", [reduction, id], function (error, results) {
                     if (error) throw error;
                     console.log("\nCongratulations! Your purchase was succesful! \n");
-                    // console.log("Your total is: " + parseFloat(quantity * results[4].price).toFixed(2) + "\n");
-                    // console.log(results);
+                    console.log("\n Your total is: " + parseFloat((quantity * price),2) + "\n");
                     loadTable();
 
                 });
@@ -68,8 +64,3 @@ function loadTable() {
     })
 }
 
-// displayResults();
-
-//Why isn't my 2nd table printing an updated balance?
-//why when I run the same purchase through the console does it not reduce the item by the same amount?
-//why is my total not summing correctly row 46? It's like it is deducting another id from the table.
